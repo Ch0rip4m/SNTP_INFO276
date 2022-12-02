d4245ac9bb09b660f18a15377673c9d3279ab707
Ch0rip4m <65402937+Ch0rip4m@users.noreply.github.com>
2022-12-02 00:34:02 -0300

    actualizacion de comentarios

--- server_sntp.py
+++ server_sntp.py
@@ -5,27 +5,27 @@ import time
 
 TIME1970 = 2208988800  #tiempo trasncurrido desde el 01/01/1970
 
-ntp = ntpl.NTPClient()
+ntp = ntpl.NTPClient() # inicializacion de ntplib
 
 PORT = 8888
-sock = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
+sock = skt.socket(skt.AF_INET, skt.SOCK_DGRAM) #conexion UDP
 sock.bind(('',PORT))
 
 print('CONEXIÓN UDP ESTABLECIDA...')
 
 while True:
-    data, clientAdress = sock.recvfrom(1024)
+    mensaje, clientAdress = sock.recvfrom(1024) # recepcion del mensaje y direccion del cliente
 
     solicitud = ntp.request('2.cl.pool.ntp.org')
-    tiempo = solicitud.tx_time
-    data = solicitud.to_data()
-    data_bytes = bytearray(data)
-    new_time_data = stc.pack('!1I', TIME1970 + int(tiempo))
-    new_time_data_bytes = bytearray(new_time_data)
-    data_bytes[40:43] = new_time_data_bytes
+    tiempoServidor = solicitud.tx_time #timestamp cliente
+    mensaje = solicitud.to_data()
+    msj_bytes = bytearray(mensaje) #codificacion para respuesta a cliente
+    nuevoTiempo = stc.pack('!1I', TIME1970 + int(tiempoServidor)) 
+    nuevoTiempo_bytes = bytearray(nuevoTiempo)
+    msj_bytes[40:43] = nuevoTiempo_bytes
     
-    solicitud.from_data(data_bytes)
+    solicitud.from_data(msj_bytes)
     
-    print('Tiempo: {}'.format(time.ctime(tiempo)))
+    print('TIEMPO SERVIDOR: {}'.format(time.ctime(tiempoServidor)))
 
-    sock.sendto(data_bytes,clientAdress)
+    sock.sendto(msj_bytes,clientAdress)
