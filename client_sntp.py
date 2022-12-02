98e695b77fe8cb9b171f849d64d989bf6bf50a77
Ch0rip4m <65402937+Ch0rip4m@users.noreply.github.com>
2022-12-02 02:41:14 -0300

    comentarios adicionales

--- client_sntp.py
+++ client_sntp.py
@@ -13,8 +13,8 @@ sock = skt.socket(skt.AF_INET, skt.SOCK_DGRAM)
 i = 0
 while i<10: 
     data = "holi"
-    sock.sendto(data.encode(), (HOST, PORT))
-    data = sock.recv(1024)
+    sock.sendto(data.encode(), (HOST, PORT)) # envio de paquete al servidor
+    data = sock.recv(1024) #recepcion de respuesta de servidor
 
     stats.from_data(data)
     print('TIEMPO RECIBIDO: {}'.format(time.ctime(stats.tx_time)))
